63f8ad2afd4afb5eaec29e56bf2aab82d42d9175
Jason Hall <jasonbrianhall@gmail.com>
2023-09-20 10:01:01 -0400

    Converted to a funtion

--- countnumberastext.py
+++ countnumberastext.py
@@ -4,8 +4,8 @@ ones = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", 
 teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 tens = ["zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
-for i in range(0, 2000001):
-
+def numbertotext(i):
+    returnstring=""
     for count in -1, 0,1,2:
         if count==-1:
             ht=int(i/1000000000)%1000
@@ -16,30 +16,33 @@ for i in range(0, 2000001):
         if count==2:
             ht=i%1000
             if i==0:
-                print(ones[i], end=" ")
+                #print(ones[i], end=" ")
+                returnstring=returnstring+ones[i] + " "
 
         tensdata=ht%100
         hundredsdata=int(ht/100)
 
         if hundredsdata>0:
-            print(ones[hundredsdata], "hundred", end=" ")
+            returnstring=returnstring+ones[hundredsdata]+ " hundred "
         if tensdata>0 and tensdata<10:
-            print(ones[tensdata], end=" ")
+            returnstring=returnstring+ ones[tensdata] + " "
         if tensdata>=10 and tensdata<20:
             temp=tensdata%10
-            print(teens[temp], end=" ")
+            returnstring=returnstring+ teens[temp] + " "
         if tensdata>=20 and tensdata<100:
             temp=int(tensdata/10)%10
             temp2=tensdata%10
             if temp2>0:
-                print(tens[temp], ones[temp2], end=" ")
+                returnstring=returnstring+ tens[temp] + " " + ones[temp2]
             else:
-                print(tens[temp], end=" ")
+                returnstring=returnstring+ tens[temp] + " "
         if count==1 and ht>0:
-            print("thousand", end=" ")
+            returnstring=returnstring + "thousand "
         if count==0 and ht>0:
-            print("million", end=" ")
+            returnstring=returnstring + "million "
         if count==-1 and ht>0:
-            print("billion", end=" ")
-    print("")
+            returnstring=returnstring + "billion "
+    return returnstring.strip()
 
+for i in range(0, 200000001):
+    print(numbertotext(i))
